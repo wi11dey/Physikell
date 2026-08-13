029a0a3e92008f8f167193bb3158626b305be19b
Will Dey <wdey@palantir.com>
2026-08-13 15:59:27 +0100

    more

---
+++ README.md
@@ -0,0 +1,3 @@
+# Physics in Haskell
+
+All four fundamental forces of nature (electromagnetism, weak nuclear force, strong nuclear force, and gravity) in Haskell.
---
+++ Setup.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Main (main) where
+
+import Control.Exception (SomeException, displayException, try)
+import Control.Monad (void)
+import Data.GraphViz.Commands (GraphvizOutput (Svg), isGraphvizInstalled, runGraphviz)
+import Data.GraphViz.Types (parseDotGraph)
+import Data.GraphViz.Types.Canonical (DotGraph)
+import qualified Data.Text.Lazy as Text
+import Distribution.Simple
+import Graphmod (graphmod)
+import System.IO (hPutStrLn, stderr)
+import System.IO.Silently (capture)
+
+main ∷ IO ()
+main =
+  defaultMainWithHooks simpleUserHooks
+  { postBuild = \args flags packageDescription localBuildInfo → do
+      postBuild simpleUserHooks args flags packageDescription localBuildInfo
+      (output, ()) ← capture $ graphmod ["--quiet"]
+      writeFile "Modules.dot" output
+      hasGraphviz ← isGraphvizInstalled
+      if hasGraphviz then do
+        void $ runGraphviz (parseDotGraph (Text.pack output) ∷ DotGraph String) Svg "Modules.svg"
+      else pure ()
+  }
