3527020b7e341fe54cd8391218d45a88c12d16e0
Will Dey <wdey@palantir.com>
2026-08-17 15:55:53 +0100

    more

--- Physics/DimensionalAnalysis.hs
+++ Physics/DimensionalAnalysis.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Physics.DimensionalAnalysis
   ( One,
     Metre,
@@ -48,6 +50,10 @@ module Physics.DimensionalAnalysis
     henry,
     (*<),
     (>+<),
+    (>-<),
+    (>*<),
+    (>/<),
+    square,
     ħ,
     mₑ,
     module Physics.Units.Arithmetic,
@@ -55,9 +61,10 @@ module Physics.DimensionalAnalysis
   )
 where
 
+import Data.Coerce
 import Data.Functor
 import Physics.Units.Type
-import Physics.Units.Arithmetic hiding ((*<), (>+<))
+import Physics.Units.Arithmetic hiding ((*<), (>+<), (>-<), (>*<), (>/<), square, hypercube)
 import Physics.Units.Constants
 import Physics.Units.Convert
 import Physics.Units.Planck ( One,
@@ -85,15 +92,17 @@ import Physics.Units.Planck ( One,
     Weber,
     Henry,
   )
-import Algebra.Ring hiding (one)
+import Algebra.Ring 
+import Algebra.Field
+import qualified Algebra.Field as Field
 import qualified Algebra.Ring as Ring
 import qualified Algebra.Transcendental as Transcendental
 import qualified Algebra.Absolute as Absolute
 import MathObj.Wrapper.NumericPrelude
 import Control.Applicative
+import Data.Proxy
+import GHC.TypeLits
 import NumericPrelude
-
-infixl 7 *<
 
 metre     ∷ Ring.C a ⇒ Metre     a; metre     = Planck 1
 kilogram  ∷ Ring.C a ⇒ Kilogram  a; kilogram  = Planck 1
@@ -119,14 +128,32 @@ ohm       ∷ Ring.C a ⇒ Ohm       a; ohm       = Planck 1
 weber     ∷ Ring.C a ⇒ Weber     a; weber     = Planck 1
 henry     ∷ Ring.C a ⇒ Henry     a; henry     = Planck 1
 
+infixl 6 >+<, >-<
+infixl 7 >*<, *<
+
 (*<) ∷ (Ring.C x, Functor f) ⇒ x → f x → f x
 x *< y = (x*) <$> y
 
+(>*<) :: (Ring.C x, Coercible (f x) x, Coercible (f' x) x, Applicative (f>*<f')) => f x -> f' x -> (f >*< f') x
+x >*< y = pure (coerce x * coerce y)
+
+(>/<) :: (Field.C x, Coercible (f x) x, Coercible (f' x) x, Applicative (f>/<f')) => f x -> f' x -> (f >/< f') x
+x >/< y = pure (coerce x / coerce y)
+
 (>+<) ∷ (Ring.C x, Applicative f) ⇒ f x → f x → f x
 x >+< y = (+) <$> x <*> y
+
+(>-<) ∷ (Ring.C x, Applicative f) ⇒ f x → f x → f x
+x >-< y = (-) <$> x <*> y
+
+square :: (Coercible (f x) (Square f x), Ring.C x, Functor f) => f x -> Square f x
+square = hypercube (Proxy :: Proxy 2)
 
 ħ ∷ Ring.C n ⇒ (Joule >*< Second) n
 ħ = undefined -- decons <$> fromSI reducedPlanckConstant
 
 mₑ ∷ Transcendental.C n ⇒ Kilogram n
 mₑ = undefined -- decons <$> fromSI electronMass
+
+hypercube :: (KnownNat n, Ring.C x, Functor f, Coercible (f x) ((f^+n) x)) => Proxy n -> f x -> (f^+n) x
+hypercube p = coerce . fmap (^natVal p)
