e35dbfa4761009dad3d36148fc94d9b7b79042b3
Will Dey <wdey@palantir.com>
2026-08-18 14:43:06 +0100

    more

--- Mathematics/Algebra/GradedRing.hs
+++ Mathematics/Algebra/GradedRing.hs
@@ -24,6 +24,7 @@ import Data.Kind (Type)
 import Data.Proxy (Proxy)
 import GHC.TypeLits (KnownNat, Nat, natVal)
 import qualified GHC.TypeNats as Nat
+import qualified Numeric.NumType.DK.Integers as TypeInt
 import qualified Numeric.Units.Dimensional as Dimensional
 import qualified Numeric.Units.Dimensional.Coercion as Dimensional
 import Prelude (Integer)
@@ -53,16 +54,24 @@ class Grade (ring :: kind -> Type -> Type) where
   type Product ring (left :: kind) (right :: kind) :: kind
   type Power ring (grade :: kind) (exponent :: Nat) :: kind
 
-type family QuantityPower (grade :: Dimensional.Dimension) (exponent :: Nat) :: Dimensional.Dimension where
-  QuantityPower grade 0 = Dimensional.DOne
-  QuantityPower grade exponent =
-    grade Dimensional.* QuantityPower grade (exponent Nat.- 1)
+type family NatToTypeInt (number :: Nat) :: TypeInt.TypeInt where
+  NatToTypeInt 0 = 'TypeInt.Zero
+  NatToTypeInt 1 = 'TypeInt.Pos1
+  NatToTypeInt 2 = 'TypeInt.Pos2
+  NatToTypeInt 3 = 'TypeInt.Pos3
+  NatToTypeInt 4 = 'TypeInt.Pos4
+  NatToTypeInt 5 = 'TypeInt.Pos5
+  NatToTypeInt 6 = 'TypeInt.Pos6
+  NatToTypeInt 7 = 'TypeInt.Pos7
+  NatToTypeInt 8 = 'TypeInt.Pos8
+  NatToTypeInt 9 = 'TypeInt.Pos9
+  NatToTypeInt number = 'TypeInt.Pos10Plus (number Nat.- 10)
 
 instance Grade Dimensional.Quantity where
   type Unit Dimensional.Quantity = Dimensional.DOne
   type Product Dimensional.Quantity left right = left Dimensional.* right
   type Power Dimensional.Quantity grade exponent =
-    QuantityPower grade exponent
+    grade Dimensional.^ NatToTypeInt exponent
 
 instance Ring.C coefficient => C Dimensional.Quantity coefficient where
   left * right =
